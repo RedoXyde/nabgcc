c2e230e10f10a81ecae4a991b3be8a90897bdbda
Redox <git@redox.ws>
2015-09-07 10:49:54 +0200

    [I2C] Cleanup

--- inc/i2c.h
+++ inc/i2c.h
@@ -1,17 +1,10 @@
 #ifndef _I2C_H_
 #define _I2C_H_
 
-#include "ml674061.h"
-#include "common.h"
-
-/***************/
-/* I2C address */
-/***************/
+/* I2C Slave address */
 #define CRX14_ADDR		0xA0
 
-/********************/
-/* SPECIFIC DEFINES */
-/********************/
+/* CRX14 settings */
 #define I2C_WRITE_INSTR		0x00
 #define I2C_READ_INSTR		0x01
 
@@ -20,9 +13,6 @@
 #define CRX14_AUTHENTICATE_REGISTER   0x02
 #define CRX14_SLOT_MARKER_REGISTER    0x03
 
-/*************/
-/* Functions */
-/*************/
 void init_i2c(void);
 uint8_t  write_i2c(uint8_t  addr_i2c, uint8_t  *data, uint8_t  nb_byte);
 uint8_t  read_i2c(uint8_t  addr_i2c, uint8_t  *data, uint8_t  nb_byte);
