151f72dc82144093ab43917122916058f6d7e43d
Redox <git@redox.ws>
2015-09-18 10:57:37 +0200

    RT2501_io cleanup, doc

--- inc/usb/rt2501usb_io.h
+++ inc/usb/rt2501usb_io.h
@@ -3,16 +3,17 @@
  * @author Sebastien Bourdeauducq - 2006 - Initial version
  * @author RedoX <dev@redox.ws> - 2015 - GCC Port, cleanup
  * @date 2015/09/07
- * @brief RT2501 Wifi/Network driver
+ * @brief RT2501 Low level I/O access
  */
 #ifndef __RT2501_IO_H
 #define __RT2501_IO_H
 
 uint32_t rt2501_read(PDEVINFO dev, uint16_t reg);
 uint8_t rt2501_write(PDEVINFO dev, uint16_t reg, uint32_t val);
-uint8_t rt2501_read_eeprom(PDEVINFO dev, uint16_t address, void *buf, uint16_t len);
+uint8_t rt2501_read_eeprom(PDEVINFO dev, uint16_t address,
+                           void *buf, uint16_t len);
 uint8_t rt2501_read_bbp(PDEVINFO dev, uint8_t reg);
 uint8_t rt2501_write_bbp(PDEVINFO dev, uint8_t reg, uint8_t val);
 uint8_t rt2501_write_rf(PDEVINFO dev, uint32_t val);
 
-#endif /* __RT2501_IO_H */
+#endif
