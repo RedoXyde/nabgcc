be4f088fb0a62fb7726d4b5aa2b53e5fbf4990b9
RedoX <git@redox.ws>
2017-02-03 00:03:14 +0100

    [AES128] Add protos. Only decrypt should be required for GTK decryption...

---
+++ src/net/aes128.c
@@ -0,0 +1,25 @@
+/**
+ * @file aes128.c
+ * @author RedoX <dev@redox.ws> - 2017
+ * @date 2017/02/02
+ * @brief RT2501 Wifi/Network driver
+ */
+#include "net/aes128.h"
+
+void aes128_init(struct aes128_context *aes, const unsigned char *key, unsigned int length)
+{
+
+}
+
+void aes128_crypt(struct aes128_context *aes, unsigned char *out,
+		const unsigned char *in, unsigned int length)
+{
+
+}
+
+void aes128_decrypt(struct aes128_context *aes, unsigned char *out,
+		const unsigned char *in, unsigned int length)
+{
+
+}
+
